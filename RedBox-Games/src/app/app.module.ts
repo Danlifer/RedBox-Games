4055275e168a07432b0590ff5eab8569e96f3be6
Danlifer <daniellillofernandez@gmail.com>
2022-06-02 10:43:58 +0200

    Main Structure

--- RedBox-Games/src/app/app.module.ts
+++ RedBox-Games/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideogameDetailComponent } from './videogame-detail/videogame-detail.component';
+import { VideogameListComponent } from './videogame-list/videogame-list.component';
+import { VideogameSearchComponent } from './videogame-search/videogame-search.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideogameDetailComponent,
+    VideogameListComponent,
+    VideogameSearchComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ RedBox-Games/src/app/videogame-search/videogame-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videogame-search',
+  templateUrl: './videogame-search.component.html',
+  styleUrls: ['./videogame-search.component.css']
+})
+export class VideogameSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
