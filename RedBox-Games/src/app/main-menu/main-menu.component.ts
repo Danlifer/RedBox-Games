00a0559310afd09ffae81a11a2486ebcba2870a9
Alberto Peña Gómez <albertopg2001@gmail.com>
2022-06-07 12:17:58 +0200

    Main Menu implemented

--- RedBox-Games/src/app/main-menu/main-menu.component.ts
+++ RedBox-Games/src/app/main-menu/main-menu.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Videogame } from '../models/videogame';
+import { VideogameService } from '../services/videogame.service';
 
 @Component({
-  selector: 'app-main-menu',
+  selector: 'main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+  videogames: Videogame[] = [];
+
+  constructor(private videogameService:VideogameService) { }
 
   ngOnInit(): void {
+    this.getData();
+    
+    setTimeout(() => {
+      this.videogames = this.videogames.slice(0, 4);
+    }, 500);
+  }
+
+  getData() {
+    //return this.videogameService.getVideogames(); 
+    this.videogameService.getVideogames()
+        .subscribe(videogames => this.videogames = videogames);
   }
 
 }
--- RedBox-Games/src/app/routing/app-routing.module.ts
+++ RedBox-Games/src/app/routing/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from '../main-menu/main-menu.component';
 import { VideogameDetailComponent } from '../videogame-detail/videogame-detail.component';
 import { VideogameHomeComponent } from '../videogame-home/videogame-home.component';
 
 const routes: Routes = [
   { path: 'detail/:id', component: VideogameDetailComponent },
-  { path: 'home', component: VideogameHomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component:  MainMenuComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'list', component:  VideogameHomeComponent},
+  { path: '**',  component: VideogameHomeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
